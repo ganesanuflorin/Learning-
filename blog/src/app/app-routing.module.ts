927573a45a91571ddb99c5778f34558076f76076
Gigi <ganesanuflorin@yahoo.com>
2022-05-25 14:28:14 +0300

    Angular homework

---
+++ blog/src/app/app-routing.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { ArticlesListComponent } from './pages/articles-list/articles-list.component';
+import { ArticleDetailComponent } from './pages/article-detail/article-detail.component';
+
+const routes: Routes = [
+  { path: 'articles', component: ArticlesListComponent },
+  { path: 'articles/:id', component: ArticleDetailComponent },
+  { path: '', redirectTo: '/articles', pathMatch: 'full' },
+];
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
